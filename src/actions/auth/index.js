91bd97d082f32977890ab8c681cd2800dfc86777
Silke Van Meerbeek <silkvanm1@student.arteveldehs.be>
2021-04-13 15:35:58 +0200

    jwt

--- src/actions/auth/index.js
+++ src/actions/auth/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-undef */
 import Express from 'express';
 import passport from 'passport';
 import passportLocal from 'passport-local';
 import UserDb from '../../lib/userDb.js';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 
 const app = Express.Router();
 const userData = new UserDb();
-userData.findOne
+dotenv.config();
 
 const LocalStrategy = passportLocal.Strategy;
 
@@ -26,7 +30,7 @@ passport.use(new LocalStrategy({
         }
 
         // check on valid pass
-        if (user.password != password) {
+        if (!await isPasswordValid(password, user.password)) {
             return done(null, false, { message: 'password incorrect' });
         }
 
@@ -44,18 +48,39 @@ app.post('/login', (req, res) => {
         } else if(!user) {
             res.status(401).json(info);
         } else {
+            const token = jwt.sign(user, process.env.JWT_UNIQUE_KEY,{
+                expiresIn: parseInt(process.env.JWT_LIFETIME)
+            });
+
             res.status(200).json({
                 succes: true,
-                token: 'thisisareallyuniquetoken'
+                token: token,
+                user: {
+                    id: user.id,
+                    username: user.email
+                }
             });
         }
     }) (req, res);
 });
 
-app.post('/', (req, res) => {
+app.post('/hashpass', (req, res)=> {
+    bcrypt.hash(req.body.password, parseInt(process.env.BCRYPT_SALT_ROUNDS)).then(function(hash) {
+        res.status(200).send(hash);
+        // store hash in password db
+    });
+
+})
+
+app.get('/', (req, res) => {
     res.status(200).json({
         "message": "Everthing is just fine"
     });
 })
 
 export default app;
+
+const isPasswordValid = async (userPassword, dbPassword) => {
+    const match = await bcrypt.compare(userPassword, dbPassword);
+    return match;
+}
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,41 @@
-export default (req, res, next) => {
+/* eslint-disable no-undef */
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
+import dotenv from 'dotenv';
 
-    // is token valid
-    const isUserAuthenticated = false;
-    
-    (isUserAuthenticated) ? next() : res.status(401).json({
-        error: "The user has no permissons"
-    });
+dotenv.config();
+
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_UNIQUE_KEY
+};
+
+// config of passport jwt
+
+passport.use(new JWTStrategy(jwtOptions, function(jwt_payload, done) {
+    try {
+        console.error(`${jwt_payload.username}somebody is doing a request`);
+        return done(null, jwt_payload.username);
+    } catch (error) {
+        return done(null, error);
+    }
+}));
+
+
+export default (req, res, next) => {
+    if(req.method == 'GET') {
+        next();
+        return false
+    }
+    passport.authenticate('jwt', {session: false}, (error, user, info) => {
+        if(error || !user) {
+            console.error('User not authenticated');
+            res.status(401).send(info);
+        } else {
+            next();
+        }
+    })(req, res, next);
 }
